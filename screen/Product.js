3716317433446307d699de4b43e8eba185b6fcc9
atsed27 <danielnigatu09@gmail.com>
2024-03-21 04:23:54 -0300

    selct color and size of product is done

--- screen/Product.js
+++ screen/Product.js
@@ -7,7 +7,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import ProductNav from '../components/ProductNav';
 import slider from '../data/slider';
 import ProductSlider from '../components/ProductSlider';
@@ -15,122 +15,329 @@ import { AntDesign } from '@expo/vector-icons';
 import COLORS from '../constance/Color';
 
 const Product = () => {
-  return (
-    <ScrollView>
-      <ProductNav />
-      <View style={styles.main}>
-        <FlatList
-          data={slider}
-          renderItem={({ item }) => <ProductSlider item={item} />}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          pagingEnabled
-          bounces={false}
-        />
-      </View>
-      <View
-        style={{
-          marginTop: 20,
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-        }}
-      >
-        <TouchableOpacity style={styles.select}>
-          <Text style={styles.selectText}>Size</Text>
-          <AntDesign
-            style={styles.selectText}
-            name="down"
-            size={12}
-            color="black"
-          />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.select}>
-          <Text style={styles.selectText}> CLOOR</Text>
-          <AntDesign
-            style={styles.selectText}
-            name="down"
-            size={12}
-            color="black"
-          />
-        </TouchableOpacity>
+  const [size, setSize] = useState(false);
+  const [color, setColor] = useState(false);
+  const [sizeVal, setSizeVal] = useState('Size');
+  const [colorVal, setColorVal] = useState('color');
 
-        <TouchableOpacity>
-          <AntDesign name="hearto" size={24} color={COLORS.black} />
-        </TouchableOpacity>
-      </View>
-      <View style={{ marginTop: 20, marginHorizontal: 25 }}>
+  const selectSize = (val) => {
+    setSizeVal(val);
+    setSize(!size);
+  };
+  const selectColor = (val) => {
+    setColorVal(val);
+    setColor(!color);
+  };
+  const colorChang = () => {
+    setColor(!color);
+    setSize(false);
+  };
+
+  return (
+    <>
+      <ScrollView>
+        <ProductNav />
+        <View style={styles.main}>
+          <FlatList
+            data={slider}
+            renderItem={({ item }) => <ProductSlider item={item} />}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            pagingEnabled
+            bounces={false}
+          />
+        </View>
         <View
           style={{
+            marginTop: 20,
             flexDirection: 'row',
-            justifyContent: 'space-between',
+            justifyContent: 'space-around',
           }}
         >
-          <Text style={{ fontSize: 22, fontWeight: '700' }}>H&M</Text>
-          <Text style={{ fontSize: 22, fontWeight: '700' }}>$19.99</Text>
+          <TouchableOpacity
+            onPress={() => {
+              setSize(!size);
+              setColor(false);
+            }}
+            style={styles.select}
+          >
+            <Text style={styles.selectText}>{sizeVal}</Text>
+            <AntDesign
+              style={styles.selectText}
+              name="down"
+              size={12}
+              color="black"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={colorChang} style={styles.select}>
+            <Text style={styles.selectText}>{colorVal}</Text>
+            <AntDesign
+              style={styles.selectText}
+              name="down"
+              size={12}
+              color="black"
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <AntDesign name="hearto" size={24} color={COLORS.black} />
+          </TouchableOpacity>
         </View>
-        <View>
-          <Text style={{ fontWeight: '100' }}>Short black dress</Text>
-          <View style={{ flexDirection: 'row' }}>
-            <TouchableOpacity>
-              <AntDesign
-                style={{ marginRight: 2 }}
-                name="star"
-                size={15}
-                color="#FFBA49"
-              />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <AntDesign
-                style={{ marginRight: 2 }}
-                name="star"
-                size={15}
-                color="#FFBA49"
-              />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <AntDesign
-                style={{ marginRight: 2 }}
-                name="star"
-                size={15}
-                color="#FFBA49"
-              />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <AntDesign
-                style={{ marginRight: 2 }}
-                name="star"
-                size={15}
-                color="#FFBA49"
-              />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <AntDesign
-                style={{ marginRight: 2 }}
-                name="star"
-                size={15}
-                color="#FFBA49"
-              />
-            </TouchableOpacity>
-            <Text>(10)</Text>
+        <View style={{ marginTop: 20, marginHorizontal: 25 }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Text style={{ fontSize: 22, fontWeight: '700' }}>H&M</Text>
+            <Text style={{ fontSize: 22, fontWeight: '700' }}>$19.99</Text>
           </View>
-        </View>
-        <View style={{ marginTop: 10 }}>
-          <Text>
-            Short dress in soft cotton jersey with decorative button downs the
-            front and a wide,frill-trimmed
-          </Text>
-        </View>
-        <TouchableOpacity style={styles.buttonCon}>
-          <View style={styles.Button}>
-            <Text
-              style={{ fontWeight: '500', fontSize: 17, color: COLORS.white }}
-            >
-              ADD TO CART
+          <View>
+            <Text style={{ fontWeight: '100' }}>Short black dress</Text>
+            <View style={{ flexDirection: 'row' }}>
+              <TouchableOpacity>
+                <AntDesign
+                  style={{ marginRight: 2 }}
+                  name="star"
+                  size={15}
+                  color="#FFBA49"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <AntDesign
+                  style={{ marginRight: 2 }}
+                  name="star"
+                  size={15}
+                  color="#FFBA49"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <AntDesign
+                  style={{ marginRight: 2 }}
+                  name="star"
+                  size={15}
+                  color="#FFBA49"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <AntDesign
+                  style={{ marginRight: 2 }}
+                  name="star"
+                  size={15}
+                  color="#FFBA49"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <AntDesign
+                  style={{ marginRight: 2 }}
+                  name="star"
+                  size={15}
+                  color="#FFBA49"
+                />
+              </TouchableOpacity>
+              <Text>(10)</Text>
+            </View>
+          </View>
+          <View style={{ marginTop: 10 }}>
+            <Text>
+              Short dress in soft cotton jersey with decorative button downs the
+              front and a wide,frill-trimmed
             </Text>
           </View>
+          <TouchableOpacity style={styles.buttonCon}>
+            <View style={styles.Button}>
+              <Text
+                style={{ fontWeight: '500', fontSize: 17, color: COLORS.white }}
+              >
+                ADD TO CART
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+      {size && (
+        <TouchableOpacity
+          onPress={() => setSize(!setSize)}
+          style={styles.selectContainer}
+        >
+          <View
+            style={{
+              marginTop: 20,
+              flexDirection: 'row',
+              justifyContent: 'center',
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: '600' }}>Select size</Text>
+          </View>
+          <View style={styles.sizeContainer}>
+            <View style={styles.conS}>
+              <TouchableOpacity
+                onPress={() => selectSize('XS')}
+                style={styles.size}
+              >
+                <Text style={{ fontSize: 16 }}>XS</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => selectSize('S')}
+                style={styles.size}
+              >
+                <Text style={{ fontSize: 16 }}>S</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => selectSize('M')}
+                style={styles.size}
+              >
+                <Text style={{ fontSize: 16 }}>M</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => selectSize('L')}
+                style={styles.size}
+              >
+                <Text style={{ fontSize: 16 }}>L</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => selectSize('XL')}
+                style={styles.size}
+              >
+                <Text style={{ fontSize: 16 }}>XL</Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                marginTop: 10,
+                borderBottomWidth: 0.5,
+                borderTopWidth: 0.5,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+            >
+              <Text style={{ paddingVertical: 8 }}>Size Info</Text>
+              <AntDesign
+                name="right"
+                size={15}
+                color={COLORS.black}
+                style={{ paddingVertical: 8 }}
+                onPress={() => navigation.navigate('')}
+              />
+            </View>
+            <View
+              style={{
+                marginTop: 15,
+                flexDirection: 'row',
+                justifyContent: 'center',
+                backgroundColor: COLORS.primary,
+                borderRadius: 20,
+              }}
+            >
+              <Text
+                style={{
+                  paddingVertical: 10,
+                  color: 'white',
+                  fontSize: 15,
+                  fontWeight: 600,
+                }}
+              >
+                ADD TO CART
+              </Text>
+            </View>
+          </View>
         </TouchableOpacity>
-      </View>
-    </ScrollView>
+      )}
+
+      {color && (
+        <TouchableOpacity
+          onPress={() => setColor(!color)}
+          style={styles.selectContainer}
+        >
+          <View
+            style={{
+              marginTop: 20,
+              flexDirection: 'row',
+              justifyContent: 'center',
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: '600' }}>
+              Select color
+            </Text>
+          </View>
+          <View style={styles.sizeContainer}>
+            <View style={styles.conS}>
+              <TouchableOpacity
+                onPress={() => selectColor('Black')}
+                style={styles.size}
+              >
+                <Text style={{ fontSize: 16 }}>Black</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => selectColor('White')}
+                style={styles.size}
+              >
+                <Text style={{ fontSize: 16 }}>White</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => selectColor('Red')}
+                style={styles.size}
+              >
+                <Text style={{ fontSize: 16 }}>Red</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => selectColor('Green')}
+                style={styles.size}
+              >
+                <Text style={{ fontSize: 16 }}>Green</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => selectColor('Blue')}
+                style={styles.size}
+              >
+                <Text style={{ fontSize: 16 }}>Blue</Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                marginTop: 10,
+                borderBottomWidth: 0.5,
+                borderTopWidth: 0.5,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+            >
+              <Text style={{ paddingVertical: 8 }}>Color Info</Text>
+              <AntDesign
+                name="right"
+                size={15}
+                color={COLORS.black}
+                style={{ paddingVertical: 8 }}
+                onPress={() => navigation.navigate('')}
+              />
+            </View>
+            <View
+              style={{
+                marginTop: 15,
+                flexDirection: 'row',
+                justifyContent: 'center',
+                backgroundColor: COLORS.primary,
+                borderRadius: 20,
+              }}
+            >
+              <Text
+                style={{
+                  paddingVertical: 10,
+                  color: 'white',
+                  fontSize: 15,
+                  fontWeight: 600,
+                }}
+              >
+                ADD TO CART
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      )}
+    </>
   );
 };
 
@@ -161,5 +368,40 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  selectContainer: {
+    backgroundColor: '#C5B5B5',
+    paddingVertical: 4,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: '38%',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+  },
+  sizeContainer: {
+    marginHorizontal: 20,
+    marginTop: 20,
+
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  conS: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+  },
+  size: {
+    width: 90,
+    backgroundColor: 'white',
+    height: 30,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 4,
+    borderWidth: 0.5,
+    borderRadius: 10,
+    marginRight: 10,
+    marginTop: 10,
   },
 });
