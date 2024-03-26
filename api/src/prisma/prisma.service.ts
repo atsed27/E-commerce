745c39259274dd7c3bf70ee42a7fc2deadbd0f8f
atsed27 <danielnigatu09@gmail.com>
2024-03-26 16:02:10 -0300

    prism connect nest

---
+++ api/src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://myuser:mypassword@localhost:5432/EcomApp?schema=public',
+        },
+      },
+    });
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
